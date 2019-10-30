b190e13b3aa1b38ba9b7462befb84372851d908f
刘石勇 <blankgrid.lsy@gmail.com>
2019-10-30 16:03:46 +0800

    更改脚本

--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
 ng build --prod;
-cp ./404.html ./dist/today-ng/404.html;
+cp ./404.html ./dist/wktodo/404.html;
 git add dist --force;
 git commit -m "demo: build demo";
-git subtree push --prefix dist/today-ng origin gh-pages;
+git subtree push --prefix dist/wktodo origin gh-pages;
